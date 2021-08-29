2d32ee5b2b2c2209e5781f021043e03a38c477c6
Vishwanath Bondugula <vishwanath5854@outlook.com>
2021-08-29 21:10:44 +0530

    PORT

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express    = require('express');
 const app        = require('express')();
-const port       = 3000 || process.env.port;
+const port       = 3000 || process.env.PORT;
 const dB         = require('./models/dB').initialiseDbConnection;
 const userRouter = require('./routes/user');
 const iwrRouter  = require('./routes/iwr');
